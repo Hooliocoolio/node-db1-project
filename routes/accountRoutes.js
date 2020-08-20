b10fbc7124c137262bdafa575e3cbf165623daf5
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-08-20 14:32:27 -0700

    working on endpoint

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -97,5 +97,18 @@ router.delete("/:id", async (req, res, next) => {
     }
 })
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+router.get("/accounts/big", (req, res, next) => {
+    try {
+        // const [accounts] = await db
+        // .select("name")
+        // .from("accounts")
+        // .where('budget', '>', 100.00)
+        res.send("hello");
+       
+    } catch(err) {
+        next(err)
+    }
+})
+
  
 module.exports = router;
